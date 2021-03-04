eeaafadccf551a2b3412e678b4c02fa0a5962447
Marcelo L. Pereira <marceloleandropereira@gmail.com>
2021-03-04 17:02:07 -0300

    feat(studio): Adiciona templates para a transmissão

--- src/app/studio/studio.component.ts
+++ src/app/studio/studio.component.ts
@@ -1,30 +1,54 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-studio',
   templateUrl: './studio.component.html',
   styleUrls: ['./studio.component.scss']
 })
-export class StudioComponent implements OnInit {
+export class StudioComponent {
 
   @ViewChild('studioCanvas', { static: true }) studioCanvas: ElementRef;
   @ViewChild('webcamStudio', { static: false }) webcamStudio: ElementRef<HTMLVideoElement>;
 
-  private context: CanvasRenderingContext2D;
+  public canvasSettings = {
+    width: 720,
+    height: 540
+  }
+
   private constraints = {
     audio: false,
     video: true
   };
 
-  ngOnInit(): void {}
+  private webcamOn = true;
 
   ngAfterViewInit(): void {
-    this.setCanvasContext();
     this.initWebCam();
   }
 
-  private setCanvasContext(): void {
-    this.context = this.studioCanvas.nativeElement.getContext('2d');
+  public chooseTemplate(templateNumber: number): void {
+    this.webcamOn = true;
+    if (templateNumber === 1) {
+      this.drawCam();
+    } else {
+      this.drawHalf();
+    }
+  }
+
+  public clearCanvas() {
+    const that = this;
+    this.webcamOn = false;
+    setTimeout(function() {
+      that.getCanvasContext().clearRect(0, 0, that.canvasSettings.width, that.canvasSettings.height);
+    }, 15);
+  }
+
+  public recordCanvas(): void {
+    
+  }
+
+  private getCanvasContext(): CanvasRenderingContext2D {
+    return this.studioCanvas.nativeElement.getContext('2d');
   }
 
   private initWebCam(): void {
@@ -33,10 +57,9 @@ export class StudioComponent implements OnInit {
     navigator.mediaDevices
       .getUserMedia(this.constraints)
       .then(function(stream){
-        // window['stream'] = stream; // make stream available to browser console
         that.webcamStudio.nativeElement.srcObject = stream;
         requestAnimationFrame(() => {
-          that.context.drawImage(that.webcamStudio.nativeElement, 0, 0, that.studioCanvas.nativeElement.width, that.studioCanvas.nativeElement.height);
+          that.drawCam();
         });
 
       })
@@ -45,4 +68,32 @@ export class StudioComponent implements OnInit {
       });
   }
 
+  private drawCam(): void {
+    const that = this;
+    setTimeout(function() {
+      that.drawCanvas(that.webcamStudio.nativeElement, 0, 0, that.canvasSettings.width, that.canvasSettings.height);
+      if (that.webcamOn) {
+        that.drawCam();
+      }
+    }, 10);
+  }
+
+  private drawHalf(): void {
+    const that = this;
+    setTimeout(function() {
+      that.drawCanvas(that.webcamStudio.nativeElement, 0, 150, (that.canvasSettings.width / 2), (that.canvasSettings.height / 2));
+      if (that.webcamOn) {
+        that.drawHalf();
+      }
+    }, 10);
+
+    this.getCanvasContext().beginPath();
+    this.getCanvasContext().arc(550, 270, 140, 0, 2 * Math.PI);
+    this.getCanvasContext().stroke();
+
+  }
+
+  private drawCanvas(image: CanvasImageSource, dx: number, dy: number, dw: number, dh: number): void {
+    this.getCanvasContext().drawImage(image, dx, dy, dw, dh);
+  }
 }
