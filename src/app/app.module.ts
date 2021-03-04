a5f5916b1edb3ee436f35c1eab67941fd990b9bb
Marcelo L. Pereira <marceloleandropereira@gmail.com>
2021-03-04 05:51:36 -0300

    Commit inicial criando o projeto em angular e um componente Studio

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudioComponent } from './studio/studio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
