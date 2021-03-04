486da257cc48ec02e75d68d897e576de924fba95
Marcelo L. Pereira <marceloleandropereira@gmail.com>
2021-03-04 15:14:22 -0300

    feat(studio): Cria um vídeo da webcam do usuário

--- src/app/studio/studio.component.ts
+++ src/app/studio/studio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-studio',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudioComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('studioCanvas', { static: true }) studioCanvas: ElementRef;
+  @ViewChild('webcamStudio', { static: false }) webcamStudio: ElementRef<HTMLVideoElement>;
 
-  ngOnInit(): void {
+  private context: CanvasRenderingContext2D;
+  private constraints = {
+    audio: false,
+    video: true
+  };
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.setCanvasContext();
+    this.initWebCam();
+  }
+
+  private setCanvasContext(): void {
+    this.context = this.studioCanvas.nativeElement.getContext('2d');
+  }
+
+  private initWebCam(): void {
+    const that = this;
+
+    navigator.mediaDevices
+      .getUserMedia(this.constraints)
+      .then(function(stream){
+        // window['stream'] = stream; // make stream available to browser console
+        that.webcamStudio.nativeElement.srcObject = stream;
+        requestAnimationFrame(() => {
+          that.context.drawImage(that.webcamStudio.nativeElement, 0, 0, that.studioCanvas.nativeElement.width, that.studioCanvas.nativeElement.height);
+        });
+
+      })
+      .catch(function(error) {
+        console.error('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+      });
   }
 
 }
